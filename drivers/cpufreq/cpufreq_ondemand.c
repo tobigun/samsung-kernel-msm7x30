97a630c7b95bf601c84b96f96a14f4c109d4fedc
Swetha Chikkaboraiah <schikk@codeaurora.org>
2013-11-29 12:22:07 +0530

    cpufreq: ondemand:kernel NULL pointer dereference at dbs_check_cpu
    Function dbs_check_cpu  calls __cpufreq_driver_getavg
    With  policy as IN/OUT parameter.
    There is possiblity of policy becoming NULL in
     __cpufreq_driver_getavg. Same policy is getting
    accessed in the for loop which leads to NULL pointer dereference.
    So NULL check is required for the policy
    
    CRs-fixed: 582925
    Change-Id: Ib8d8de8d66500430a5aa4c275937640e4f934aa8
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -787,6 +787,8 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 	this_dbs_info->freq_lo = 0;
 	policy = this_dbs_info->cur_policy;
+	if(policy == NULL)
+		return;
 
 	/* Only core0 controls the boost */
 	if (dbs_tuners_ins.boosted && policy->cpu == 0) {
@@ -879,6 +881,8 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		j_dbs_info->max_load  = max(cur_load, j_dbs_info->prev_load);
 		j_dbs_info->prev_load = cur_load;
 		freq_avg = __cpufreq_driver_getavg(policy, j);
+		if (policy == NULL)
+			return;
 		if (freq_avg <= 0)
 			freq_avg = policy->cur;
 
